446f14765f650da0bfcdb6a9b334cc01a72f131f
Matheus Miranda <matheus.sm86@gmail.com>
2022-11-02 20:09:15 -0300

    Fix GitHubService

--- app/src/main/java/br/com/igorbag/githubsearch/data/network/GitHubService.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/data/network/GitHubService.kt
@@ -1,12 +1,10 @@
 package br.com.igorbag.githubsearch.data.network
 
-import br.com.igorbag.githubsearch.domain.model.UserRepo
-import kotlinx.coroutines.flow.Flow
+import br.com.igorbag.githubsearch.data.network.dto.UserRepoResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
-
     @GET("users/{username}/repos")
-    fun getAllRepositoriesByUser(@Path("username") user: String): Flow<List<UserRepo>>
+    suspend fun getAllRepositoriesByUser(@Path("username") user: String): List<UserRepoResponse>
 }
