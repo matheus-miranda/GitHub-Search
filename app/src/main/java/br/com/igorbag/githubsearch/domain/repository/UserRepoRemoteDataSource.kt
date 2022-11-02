30081fd57482194187d08a2676c1ec999c942de4
Matheus Miranda <matheus.sm86@gmail.com>
2022-11-02 17:44:24 -0300

    Create UserRepoRemoteDataSource

---
+++ app/src/main/java/br/com/igorbag/githubsearch/domain/repository/UserRepoRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package br.com.igorbag.githubsearch.domain.repository
+
+import br.com.igorbag.githubsearch.domain.ResultStatus
+import br.com.igorbag.githubsearch.domain.model.UserRepo
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepoRemoteDataSource {
+    fun fetchRepoList(): Flow<ResultStatus<List<UserRepo>>>
+}
